5f7d2f2b1e539f6fe2dec59921744bcb7b0accac
Mininath47 <meninathhonmane953@gmail.com>
2024-12-26 17:12:44 +0530

    h

--- app.js
+++ app.js
@@ -1,43 +1,99 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const cors = require('cors');
-const mongodb = require('mongodb').MongoClient;
-const app = express();
-const dotenv = require('dotenv');
-dotenv.config()
-const mongoPath = process.env.MongoPath;
-const Port = process.env.PORT || 3000;
 
+const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
+const url = 'mongodb://127.0.0.1:27017'; // Replace with your MongoDB connection string
+const dbName = 'StateDrop'; // Replace with your database name
+let db;
+
+// Connect to MongoDB
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(client => {
+        console.log("Connected to MongoDB");
+        db = client.db(dbName);
+    })
+    .catch(err => console.error("Error connecting to MongoDB:", err));
+
+// Route to fetch all states
 app.get('/state', (req, res) => {
-    mongodb.connect(mongoPath).then((object) => {
-        const database = object.db('StateDrop');
-        database.collection('State').find({}).toArray().then((document) => {
-            res.send(document);
-            res.end();
+    db.collection('State').find({}).toArray()
+        .then(documents => res.send(documents))
+        .catch(err => {
+            console.error("Error fetching states:", err);
+            res.status(500).send("Internal Server Error");
         });
-    });
 });
 
-app.get('/khat', (req, res) => {
-    mongodb.connect(mongoPath).then((object) => {
-        const database = object.db('StateDrop');
-        database.collection('Khat').find({}).toArray().then((document) => {
-            res.send(document);
-            res.end();
+// Route to fetch districts by state
+app.get('/state/:stateName', (req, res) => {
+    const stateName = req.params.stateName;
+    db.collection('State').findOne({ 'states.name': stateName })
+        .then(document => {
+            if (document) {
+                const state = document.states.find(state => state.name === stateName);
+                res.send(state.districts);
+            } else {
+                res.status(404).send("State not found");
+            }
         })
-    })
-})
-app.get('/medicine', (req, res) => {
-    mongodb.connect(mongoPath).then((object) => {
-        const database = object.db('StateDrop');
-        database.collection('medicine').find({}).toArray().then((document) => {
-            res.send(document);
-            res.end();
-        })
-    })
-})
+        .catch(err => {
+            console.error("Error fetching districts:", err);
+            res.status(500).send("Internal Server Error");
+        });
+});
 
-app.listen(Port);
-console.log(`server is running... http://127.0.0.1:${Port}`);     
+// Route to fetch subdistricts by district
+app.get('/state/:stateName/:districtName', (req, res) => {
+    const { stateName, districtName } = req.params;
+    db.collection('State').findOne({ 'states.name': stateName })
+        .then(document => {
+            if (document) {
+                const state = document.states.find(state => state.name === stateName);
+                const district = state.districts.find(district => district.name === districtName);
+                if (district) {
+                    res.send(district.subdistricts);
+                } else {
+                    res.status(404).send("District not found");
+                }
+            } else {
+                res.status(404).send("State not found");
+            }
+        })
+        .catch(err => {
+            console.error("Error fetching subdistricts:", err);
+            res.status(500).send("Internal Server Error");
+        });
+});
+
+// Route to fetch villages by subdistrict
+app.get('/state/:stateName/:districtName/:subdistrictName', (req, res) => {
+    const { stateName, districtName, subdistrictName } = req.params;
+    db.collection('State').findOne({ 'states.name': stateName })
+        .then(document => {
+            if (document) {
+                const state = document.states.find(state => state.name === stateName);
+                const district = state.districts.find(district => district.name === districtName);
+                const subdistrict = district.subdistricts.find(sub => sub.name === subdistrictName);
+                if (subdistrict) {
+                    res.send(subdistrict.villages);
+                } else {
+                    res.status(404).send("Subdistrict not found");
+                }
+            } else {
+                res.status(404).send("State not found");
+            }
+        })
+        .catch(err => {
+            console.error("Error fetching villages:", err);
+            res.status(500).send("Internal Server Error");
+        });
+});
+
+// Start the server
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
